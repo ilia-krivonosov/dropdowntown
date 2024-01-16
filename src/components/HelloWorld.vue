84b297a960d7ad69adadc7704a22e50f45f98dd2
Ilia Krivonosov <ilia.krivonosov@fundraiseup.com>
2024-01-16 11:40:25 +0100

    init project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<script lang="ts">
+import {defineComponent} from "vue";
+import Multiselect from "@/components/Multiselect.vue";
+
+export default defineComponent({
+  components: {Multiselect}
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">You did it!</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+    </h3>
+  </div>
+
+  <div>
+    <Multiselect />
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/Multiselect.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <VueMultiselect v-model="selectedOption" :options="options" label="name"></VueMultiselect>
+  </div>
+</template>
+
+<script>
+import VueMultiselect from 'vue-multiselect'
+
+export default {
+  components: {
+    VueMultiselect
+  },
+  data() {
+    return {
+      selectedOption: null,
+      options: [
+        { name: 'Option 1' },
+        { name: 'Option 2' },
+        { name: 'Option 3' }
+      ]
+    }
+  }
+}
+</script>
+
+<style src="../assets/vue-multiselect.css"></style>
